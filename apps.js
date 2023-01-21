a4160bea1f21101b7512d41eda0159d33044a5f9
Jesse Gelardi <jesse.gelardi97@gmail.com>
2023-01-21 17:52:08 -0500

    last commit

--- apps.js
+++ apps.js
@@ -75,7 +75,7 @@ class Menu {
             1) create character
             2) delete character
             ----------------------------
-            ${teamInfo}
+            ${partyInfo}
          `);
     }
 
@@ -88,7 +88,7 @@ class Menu {
     }
     
     createParty() {
-        let name = prompt('Enter name for party:'):
+        let name = prompt('Enter name for party:');
         this.parties.push(new Party(name));
      }
 
@@ -96,7 +96,7 @@ class Menu {
         let index = prompt('Enter the index of the party you wish to view:');
         if (index > -1 && index < this.parties.length) {
             this.selectedParty = this.parties[index];
-            let description = 'Party Name: ' + this.selectedParty.name + '/n';
+            let description = 'Party Name: ' + this.selectedParty.name + '\n';
             
             for (let i = 0; i < this.selectedParty.characters.length; i++) {
                 description += i + ') ' + this.selectedParty.characters[i].name 
@@ -113,7 +113,22 @@ class Menu {
             }
         }
     }
+
+    createCharacter() {
+        let name = prompt('Enter name for new character:');
+        let job = prompt('Enter job class for new player');
+        this.selectedParty.characters.push(new haracter(name, job));
+    }
+
+    deleteCharacter() {
+        let index = prompt('Enter the index of the character you wish to delete:');
+        if (index > -1 && index < this.selectedParty.characters.length) {
+            this.selectedParty.characters.splice(index, 1);
+        }
+    }
 }
+
+
 
 let menu = new Menu();
 menu.start();
