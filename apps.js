15ca4401700d46038f41a1b1594aaea162bd5109
Jesse Gelardi <jesse.gelardi97@gmail.com>
2023-01-21 16:39:04 -0500

    second commit

---
+++ apps.js
@@ -0,0 +1,91 @@
+console.log('Hello!');
+
+class PlayerCharacter {
+    constructor (name, job) {
+        this.name = name;
+        this.job = job;
+    }
+    describe() {
+        return `${this.name} is a ${this.job}.`;
+    }
+}
+
+class Party {
+    constructor(name) {
+        this.name = name; 
+        this.characters = [];
+    }
+
+    addPlayerCharacter(character) {
+        if (character instanceof PlayerCharacter) {
+            this.characters.push(character);
+        } else {
+        throw new Error(`You can only add an instance of Player Character. Argument is not a Player Character: ${character}`);
+        }
+    }
+describe() {
+    return `${this.name} has ${this.characters.length} party members.`;
+    }
+}
+
+class Menu {
+    constructor() {
+        this.parties = [ ];
+        this.selectedParty = null;
+    }
+
+    start() {
+        let selection = this.showMainMenuOptions();
+        while (selection != 0) {
+            switch (selection) {
+                case '1':
+                    this.createParty();
+                    break;
+                case '2':
+                    this.viewParty();
+                    break;
+                case '3':
+                    this.deleteParty();
+                    break;
+                case '4':
+                    this.displayParties();
+                    break;
+                default:
+                    selection = 0;
+            }
+        selection = this.showMainMenuOptions();
+        }
+
+    alert ('Goodbye!');
+    }
+
+    showMainMenuOptions() {
+        return prompt(`
+        0) exit
+        1) create new party
+        2) view party
+        3) delete party
+        4) display all parties
+        `);
+    }
+
+    displayParties() {
+        let partyString = '';
+        for (let i = 0; i < this.parties.length; i++) {
+            partyString += i + ') ' + this.parties[i].name + '\n';
+        }
+        alert(partyString);
+    }
+    
+    createParty() {
+        let name = prompt('Enter name for party:'):
+        this.parties.push(new Party(name));
+     }
+
+    viewParty() {
+        let index = prompt('Enter the index of the party you wish to view:');
+        if (index > -1 && index < this.parties.length) {
+            this.selectedParty = this.parties[index];
+        }
+    }
+}
